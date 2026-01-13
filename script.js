911fedeee6631eb33769a059a2a0a9e667f83ac7
mohamed-amine-namasse <namasse.medamine@laplateforme.io>
2026-01-13 12:57:55 +0100

    commit

--- script.js
+++ script.js
@@ -190,3 +190,15 @@ document.addEventListener("DOMContentLoaded", () => {
     observer.observe(element);
   });
 });
+
+const swiper = new Swiper(".skillSwiper", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  loop: true,
+  pagination: { el: ".swiper-pagination", clickable: true },
+  navigation: { nextEl: ".swiper-button-next", prevEl: ".swiper-button-prev" },
+  breakpoints: {
+    768: { slidesPerView: 2 },
+    1024: { slidesPerView: 3 },
+  },
+});
